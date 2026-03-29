eb93b30562f7919447bf7c45c142eedc7e238c93
vansheekajain <vansheekajain26@gmail.com>
2026-03-29 08:43:31 +0530

    Create Product.java

---
+++ src/com/inventory/model/Product.java
@@ -0,0 +1,53 @@
+package com.inventory.model;
+
+public class Product {
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+
+    // Default Constructor
+    public Product() {}
+
+    // Parameterized Constructor
+    public Product(int id, String name, int quantity, double price) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
