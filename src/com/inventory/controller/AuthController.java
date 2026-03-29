7553319a7576ecd64d8ab1d41b59711b36385c65
Avnish2005Singh <130545443+Avnish2005Singh@users.noreply.github.com>
2026-03-29 10:03:13 +0530

    Update AuthController.java

--- src/com/inventory/controller/AuthController.java
+++ src/com/inventory/controller/AuthController.java
@@ -3,12 +3,24 @@ package com.inventory.controller;
 public class AuthController {
 
     public boolean login(String username, String password) {
+        try {
+            username = username.trim();
+            password = password.trim();
 
-        // Dummy login logic
-        if (username.equals("admin") && password.equals("admin")) {
-            return true;
+            if (username.isEmpty() || password.isEmpty()) {
+                throw new Exception("Fields cannot be empty");
+            }
+
+            // Dummy login
+            if (username.equals("admin") && password.equals("admin")) {
+                return true;
+            }
+
+            return false;
+
+        } catch (Exception e) {
+            System.out.println("Login Error: " + e.getMessage());
+            return false;
         }
-
-        return false;
     }
 }
