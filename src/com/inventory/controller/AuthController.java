05504006f9e8fb46a153108fc905791ef316c3cb
vansheekajain <vansheekajain26@gmail.com>
2026-03-29 08:37:27 +0530

    Update AuthController.java

--- src/com/inventory/controller/AuthController.java
+++ src/com/inventory/controller/AuthController.java
@@ -3,7 +3,12 @@ package com.inventory.controller;
 public class AuthController {
 
     public boolean login(String username, String password) {
-        // Temporary logic (dummy)
-        return username.equals("admin") && password.equals("admin");
+
+        // Dummy login logic
+        if (username.equals("admin") && password.equals("admin")) {
+            return true;
+        }
+
+        return false;
     }
 }
