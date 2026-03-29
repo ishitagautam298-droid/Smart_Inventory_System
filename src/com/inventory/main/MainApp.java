c705ff78638069967fb25fd0b529c7e8240f5663
vansheekajain <vansheekajain26@gmail.com>
2026-03-29 10:11:19 +0530

    Update MainApp.java

--- src/com/inventory/main/MainApp.java
+++ src/com/inventory/main/MainApp.java
@@ -1,9 +1,27 @@
 package com.inventory.main;
 
+import javax.swing.UIManager;
+import javax.swing.SwingUtilities;
+
 import com.inventory.ui.LoginFrame;
 
 public class MainApp {
+
     public static void main(String[] args) {
-        new LoginFrame().setVisible(true);
+
+        // Set system look and feel (modern UI)
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            System.out.println("Look and Feel not applied");
+        }
+
+        // Run UI in Event Dispatch Thread (best practice)
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new LoginFrame().setVisible(true);
+            }
+        });
     }
 }
